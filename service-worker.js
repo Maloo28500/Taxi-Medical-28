a39687de80e129c9216d03a233f53361032aa82e
Maloo28500 <maloo28500@gmail.com>
2026-07-07 16:50:08 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'taxi-medical-28-v1';
+const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
