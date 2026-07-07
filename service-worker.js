0672c8333ce6f0135c67fc2df6e5a1a38be8e63f
Maloo28500 <maloo28500@gmail.com>
2026-07-07 21:38:48 +0200

    Update cache name to 'taxi-medical-28-v2'

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taxi-medical-28-v1';
+const CACHE_NAME = 'taxi-medical-28-v2';
 const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
